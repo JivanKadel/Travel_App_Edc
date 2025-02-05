27ac827983af17efc0bf18c9602f6024b0d394ce
Jivan Kadel <jivankadel256@gmail.com>
2025-02-05 11:56:04 +0545

    Replacing android with tools for dummy data

---
+++ app/src/main/java/com/jivan/travelapp/BlogDetailsActivity.java
@@ -0,0 +1,14 @@
+package com.jivan.travelapp;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class BlogDetailsActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_blog_details);
+    }
+}
