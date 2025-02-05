b97fb8a664b1bbb2552c5dac17940449db1d7117
Jivan Kadel <jivankadel256@gmail.com>
2025-02-05 12:27:52 +0545

    Set blogDetails data from code

--- app/src/main/java/com/jivan/travelapp/BlogDetailsActivity.java
+++ app/src/main/java/com/jivan/travelapp/BlogDetailsActivity.java
@@ -1,6 +1,9 @@
 package com.jivan.travelapp;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,5 +13,36 @@ public class BlogDetailsActivity extends AppCompatActivity {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_blog_details);
+
+        ImageView imageMain = findViewById(R.id.mainImage);
+        imageMain.setImageResource(R.drawable.sydney);
+
+        ImageView imageAvatar = findViewById(R.id.imageAvatar);
+        imageAvatar.setImageResource(R.drawable.avatar);
+
+        TextView textDate = findViewById(R.id.textDate);
+        textDate.setText("August 2, 2019");
+
+        TextView textTitle = findViewById(R.id.textTitle);
+        textTitle.setText("G'day from Sydney");
+
+        TextView textAuthor = findViewById(R.id.sydney_guide_name);
+        textAuthor.setText("Grayson Wells");
+
+        TextView ratingText = findViewById(R.id.sydney_text_rating);
+        ratingText.setText("4.4");
+
+        RatingBar sydneyRating = findViewById(R.id.sydney_guide_rating);
+        sydneyRating.setRating(4.4F);
+
+        TextView totalViews = findViewById(R.id.sydney_views);
+        totalViews.setText("(2687 views)");
+
+        TextView sydneyDesc = findViewById(R.id.sydney_description);
+        sydneyDesc.setText("Australia is one of the most beautiful place on the planet.");
+
+        ImageView backIcon = findViewById(R.id.back_arrow);
+        backIcon.setOnClickListener(v->finish());
+
     }
 }
