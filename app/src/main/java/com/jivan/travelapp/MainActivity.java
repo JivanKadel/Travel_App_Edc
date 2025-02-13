8951a81d4ff8ce7fadf17101885f54a1fa1bb132
Jivan Kadel <jivankadel256@gmail.com>
2025-02-13 15:27:59 +0545

    Load data, show error snack bar

--- app/src/main/java/com/jivan/travelapp/MainActivity.java
+++ app/src/main/java/com/jivan/travelapp/MainActivity.java
@@ -2,12 +2,17 @@ package com.jivan.travelapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,5 +22,30 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         startActivity(new Intent(this, BlogDetailsActivity.class));
+    }
+    private void loadData(){
+        BlogHttpClient.INSTANCE.loadBlogArticles(new BlogArticlesCallback() {
+            @Override
+            public void onSuccess(List<Blog> blogList) {
+                // TODO
+            }
+
+            @Override
+            public void onError() {
+                runOnUiThread(()->{
+                    showErrorSnackBar();
+                });
+            }
+        });
+    }
+    private void showErrorSnackBar(){
+        View rootView = findViewById(android.R.id.content);
+        Snackbar snackbar = Snackbar.make(rootView, "Error loading Blog Data", Snackbar.LENGTH_INDEFINITE);
+        snackbar.setActionTextColor(getResources().getColor(R.color.orange500));
+        snackbar.setAction("Retry", v->{
+            loadData();
+            snackbar.dismiss();
+        });
+        snackbar.show();
     }
 }
