3ba29080e68f323dcaa138452e0f84edfdad4cd3
Jivan Kadel <jivankadel256@gmail.com>
2025-02-20 21:43:24 +0545

    Override .equals, and .hashCode methods

--- app/src/main/java/com/jivan/travelapp/Author.java
+++ app/src/main/java/com/jivan/travelapp/Author.java
@@ -1,5 +1,7 @@
 package com.jivan.travelapp;
 
+import java.util.Objects;
+
 public class Author {
     private String name;
     private String avatar;
@@ -11,7 +13,19 @@ public class Author {
     public String getAvatar() {
         return avatar;
     }
-    public String getAvatarUrl(){
+
+    public String getAvatarUrl() {
         return BlogHttpClient.BASE_URL + BlogHttpClient.PATH + getAvatar();
+    }
+
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Author author = (Author) o;
+        return Objects.equals(name, author.name) && Objects.equals(avatar, author.avatar);
+    }
+
+    public int hashCode(){
+        return Objects.hash(name, avatar);
     }
 }
--- app/src/main/java/com/jivan/travelapp/Blog.java
+++ app/src/main/java/com/jivan/travelapp/Blog.java
@@ -1,6 +1,9 @@
 package com.jivan.travelapp;
 
+import java.util.Objects;
+
 public class Blog {
+    private String id;
     private String title;
     private String date;
     private int views;
@@ -12,25 +15,55 @@ public class Blog {
     public String getTitle() {
         return title;
     }
+
     public String getDate() {
         return date;
     }
+
     public int getViews() {
         return views;
     }
+
     public float getRating() {
         return rating;
     }
+
     public String getImage() {
         return image;
     }
-    public String getImageUrl(){
+
+    public String getImageUrl() {
         return BlogHttpClient.BASE_URL + BlogHttpClient.PATH + getImage();
     }
+
     public String getDescription() {
         return description;
     }
-    public Author getAuthor(){
+
+    public Author getAuthor() {
         return author;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Blog blog = (Blog) o;
+
+        return views == blog.views && Float.compare(blog.rating, rating) == 0
+                && Objects.equals(id, blog.id)
+                && Objects.equals(author, blog.author)
+                && Objects.equals(title, blog.title)
+                && Objects.equals(date, blog.date)
+                && Objects.equals(image, blog.image)
+                && Objects.equals(description, blog.description);
+    }
+
+    public int hashCode(){
+        return Objects.hash(id, author, title, date, image, description, views, rating);
     }
 }
