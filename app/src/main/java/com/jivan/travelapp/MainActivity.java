e7a8856a8ce7e7011e34265464aca04bbd3601f3
Jivan Kadel <jivankadel256@gmail.com>
2025-02-20 21:43:49 +0545

    Bind the adapter to recycler view

--- app/src/main/java/com/jivan/travelapp/MainActivity.java
+++ app/src/main/java/com/jivan/travelapp/MainActivity.java
@@ -1,48 +1,68 @@
 package com.jivan.travelapp;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowInsets;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.snackbar.Snackbar;
+import com.jivan.travelapp.adapter.MainAdapter;
 
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    private MainAdapter adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        startActivity(new Intent(this, BlogDetailsActivity.class));
+        adapter = new MainAdapter();
+
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(adapter);
+
+//        startActivity(new Intent(this, BlogDetailsActivity.class));
+        loadData();
+
     }
-    private void loadData(){
+
+    private void loadData() {
         BlogHttpClient.INSTANCE.loadBlogArticles(new BlogArticlesCallback() {
             @Override
             public void onSuccess(List<Blog> blogList) {
-                // TODO
+                runOnUiThread(() -> {
+                    adapter.submitList(blogList);
+                });
             }
 
             @Override
             public void onError() {
-                runOnUiThread(()->{
+                runOnUiThread(() -> {
                     showErrorSnackBar();
                 });
             }
         });
     }
-    private void showErrorSnackBar(){
+
+    private void showErrorSnackBar() {
         View rootView = findViewById(android.R.id.content);
         Snackbar snackbar = Snackbar.make(rootView, "Error loading Blog Data", Snackbar.LENGTH_INDEFINITE);
         snackbar.setActionTextColor(getResources().getColor(R.color.orange500));
-        snackbar.setAction("Retry", v->{
+        snackbar.setAction("Retry", v -> {
             loadData();
             snackbar.dismiss();
         });
